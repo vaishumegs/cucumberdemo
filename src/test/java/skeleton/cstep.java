63500607d637bbdbd74b357e2ea3e67451d41af2
v.ramiya.surendran <v.ramiya.surendran@CDC2-L-J9G7QQ2.dir.svc.accenture.com>
2020-02-04 14:23:13 +0530

    new updates

---
+++ src/test/java/skeleton/cstep.java
@@ -0,0 +1,26 @@
+package skeleton;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class cstep {
+	@Given("i  some cakes in my hand")
+	public void i_some_cakes_in_my_hand() {
+	    // Write code here that turns the phrase above into concrete actions
+	   
+	}
+
+	@When("i  for sometime")
+	public void i_for_sometime() {
+	    // Write code here that turns the phrase above into concrete actions
+	   
+	}
+
+	@Then("my  disapper")
+	public void my_disapper() {
+	    // Write code here that turns the phrase above into concrete actions
+	   
+	}
+
+}
---
+++ src/test/resources/c.feature
@@ -0,0 +1,6 @@
+Feature: C
+
+Scenario: a few cakes
+Given i have 10 cakes in my hand
+When i keep for sometime
+Then my "all" disapper
